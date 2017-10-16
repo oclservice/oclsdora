829369b8a9472f26f6f79df9292b6c69b50e9f2f
MorganDawe <mdawe@discoverygarden.ca>
2017-10-16 16:29:05 -0300

    Adding form alters to add next/prev on guided ingest pages.

---
+++ js/tab_nav.js
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * Adds Next/Previous functionality.
+ */
+(function ($, Drupal, window, document, undefined) {
+
+Drupal.behaviors.islandora_guided_ingest = {
+  attach: function(context, settings) {
+    $(window).load(function(){
+      $('.islandora-guided-ingest-next').click(function(e){
+        e.preventDefault();
+        $('.vertical-tabs-list').find('li.selected').next().find('a').trigger('click');
+      });
+
+      $('.islandora-guided-ingest-prev').click(function(e){
+        e.preventDefault();
+        $('.vertical-tabs-list').find('li.selected').prev().find('a').trigger('click');
+      });
+    });
+  }
+};
+
+})(jQuery, Drupal, this, this.document);
