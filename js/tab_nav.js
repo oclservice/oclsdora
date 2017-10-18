4fc70d7c8d1a662ba519b36e3974edbea9735991
MorganDawe <mdawe@discoverygarden.ca>
2017-10-18 09:02:22 -0300

    Code review feedback.

--- js/tab_nav.js
+++ js/tab_nav.js
@@ -2,17 +2,18 @@
  * @file
  * Adds Next/Previous functionality.
  */
+
 (function ($, Drupal, window, document, undefined) {
 
 Drupal.behaviors.islandora_guided_ingest = {
-  attach: function(context, settings) {
-    $(window).load(function(){
-      $('.islandora-guided-ingest-next').click(function(e){
+  attach: function (context, settings) {
+    $(window).load(function () {
+      $('.islandora-guided-ingest-next').click(function (e) {
         e.preventDefault();
         $('.vertical-tabs-list').find('li.selected').next().find('a').trigger('click');
       });
 
-      $('.islandora-guided-ingest-prev').click(function(e){
+      $('.islandora-guided-ingest-prev').click(function (e) {
         e.preventDefault();
         $('.vertical-tabs-list').find('li.selected').prev().find('a').trigger('click');
       });
